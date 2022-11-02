a11958845239b19398daa14ceb2b0c2bdbdf6e75
Malaica Brunks <malaica.brunks@gmx.de>
2022-11-02 10:13:38 +0100

    added Array

---
+++ script.js
@@ -0,0 +1,9 @@
+let fields = [];
+
+
+function fillShape(id) {            //id steht für den Funktionsparameter 0-8, den wir in der index.html der Funktion geben
+    fields[id] = 'cross';           //das Array fields an der Stelle id(0-8) ist zB "cross"
+console.log(fields);
+
+
+}
