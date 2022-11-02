8c2c7753fd30f155c40eb31b2d556c155c43568c
Malaica Brunks <malaica.brunks@gmx.de>
2022-11-02 10:54:31 +0100

    added change player and show img

--- script.js
+++ script.js
@@ -12,6 +12,19 @@ function fillShape(id) {                //id steht für den Funktionsparameter 0
                                         
     fields[id] = currentShape;           //das Array fields an der Stelle id(0-8) ist zB "cross"
 console.log(fields);
+draw();
+}
 
 
+//Spieler wechseln
+function draw() {
+    for(let i = 0; i < fields.length; i++) {       
+       if(fields[i] == 'circle') {                                          //wenn fields an der Stelle i = "circle" ist
+        document.getElementById(`circle-${i}`).classList.remove('d-none');  //dann entferne die classlist d-none von "circle"
+       }
+
+       if(fields[i] == 'cross') {                                            //wenn fields an der Stelle i = "cross" ist
+        document.getElementById(`cross-${i}`).classList.remove('d-none');   //dann entferne die classlist d-none von "cross"
+       }
+    }
 }
